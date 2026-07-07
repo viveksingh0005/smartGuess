1603fa4d73e393e1fc174f0688b66720eba44c77
vivek <boravivek459@gmail.com>
2026-07-07 10:57:49 +0530

    bug fix in game socket file import

--- server/src/sockets/gameSocket.js
+++ server/src/sockets/gameSocket.js
@@ -1,5 +1,5 @@
 const GameRoom = require("../models/GameRoom");
-const User = require("../models/User");
+const User = require("../models/user");
 const { QUESTIONS_BANK } = require("../utils/questionBank");
 const gameSocket = (io) => {
   io.on("connection", (socket) => {
